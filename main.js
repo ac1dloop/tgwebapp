b4d3a9d54822c2b8df0411d40f97ea75e3d9ad9e
ac1dloop <rakivvode@gmail.com>
2025-11-21 20:17:27 +0100

    add canvas and lib

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+// Initialize the Telegram Web App
+Telegram.WebApp.ready()
+// Example: Display user data (if available)
+if (Telegram.WebApp.initDataUnsafe && Telegram.WebApp.initDataUnsafe.user) {
+    document.body.innerHTML += `<p>Hello, ${Telegram.WebApp.initDataUnsafe.user.first_name}!</p>`;
+}
+// Example: Close the Mini App programmatically
+// Telegram.WebApp.close();
+
+function setup() {
+  createCanvas(500, 500);
+}
+function draw() {
+  background(220);
+}
+
 console.log("Hello")
 
 elem = document.getElementById("beg")
