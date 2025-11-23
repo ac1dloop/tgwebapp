7f271ecaf1a6944aef0765b648df99e3e407061f
ac1dloop <rakivvode@gmail.com>
2025-11-23 02:36:35 +0100

    test basic image

--- main.js
+++ main.js
@@ -5,56 +5,38 @@ const txtSize = 10;
 
 let mainImage = null;
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     Telegram.WebApp.ready();
+document.addEventListener("DOMContentLoaded", () => {
+    Telegram.WebApp.ready();
 
-//     Telegram.WebApp.expand();
-// });
+    Telegram.WebApp.expand();
+});
 
-Telegram.WebApp.ready();
-Telegram.WebApp.expand();
+const canvas = document.getElementById("canv");
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+const ctx = canvas.getContext("2d");
 
-// Print Web App User info
-function printWebAppInfo(){
-textSize(txtSize);
+ctx.font = "48px serif";
+ctx.fillText("Hello world", 0, 200);
 
-let outputText = "Failed to get data";
-
-if (Telegram.WebApp.initDataUnsafe && Telegram.WebApp.initDataUnsafe.user) {
-    outputText = JSON.stringify(Telegram.WebApp.initDataUnsafe);
+function ableToLoadImage() {
+    Telegram.WebApp.initDataUnsafe && 
+    Telegram.WebApp.initDataUnsafe.user && 
+    Telegram.WebApp.initDataUnsafe.user.photo_url;
 }
 
-text(outputText, padding, padding, canvasWidth - padding, canvasHeight - padding);
+if (ableToLoadImage()) {
+    img = new Image();
+    img.src = Telegram.WebApp.initDataUnsafe.user.photo_url;
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0);  // draw at x=0, y=0
+    };
+} else {
+    img = new Image();
+    img.src = "https://mdn.github.io/shared-assets/images/examples/mdn.svg";
+        img.onload = () => {
+        ctx.drawImage(img, 0, 0);  // draw at x=0, y=0
+    };
 }
 
-function preload() {
-    if (Telegram.WebApp.initDataUnsafe && Telegram.WebApp.initDataUnsafe.user && Telegram.WebApp.initDataUnsafe.user.photo_url) {
-        mainImage = loadImage(Telegram.WebApp.initDataUnsafe.user.photo_url);
-    } else {
-        mainImage = loadImage("https://mdn.github.io/shared-assets/images/examples/mdn.svg");
-    }
-}
 
-function drawAvatar() {
-
-image(mainImage, 0, 0);
-
-}
-
-// Setup canvas
-function setup() {
-    createCanvas(canvasWidth, canvasHeight);
-    // imageMode(CENTER);
-}
-
-// Event loop
-function draw() {
-    background(220);
-
-    printWebAppInfo();
-
-    drawAvatar();
-}
-
-// Example: Close the Mini App programmatically
-// Telegram.WebApp.close();
